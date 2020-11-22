d44cf07be340d8eb6114da8564aeb8064ebc6493
Magesh.K <47920326+mahee96@users.noreply.github.com>
2020-11-22 17:06:08 +0530

    [release]  - Refer to notes from 0.0.2  + Published 0.0.4

--- release_notes/whatsnew.md
+++ release_notes/whatsnew.md
@@ -1,3 +1,12 @@
+## Released on Sun 22 Nov 2020 16:59:22  
+
+#### Whats new in this Release: 
+
+>  * _Fixed invalid dependencies specified in the install-requires file_
+>  * _Updated gitignore to include build and dist dirs_
+
+
+
 ## Released on Sun 22 Nov 2020 16:56:28  
 
 #### Whats new in this Release: 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 setup(
     name='PlayReleaseApi',         # How you named your package folder (MyLib)
     # Start with a small number and increase it with every change you make
-    version='0.0.3',
+    version='0.0.4',
     packages=setuptools.find_packages(exclude=["tests", "data"]),
     long_description=Path("README.md").read_text(),
     # Chose a license from here: https://help.github.com/articles/licensing-a-repository
@@ -17,7 +17,7 @@ setup(
     # Provide either the link to your github or to your website
     url='https://github.com/mahee96/PlayReleaseApi',
     # I explain this later on
-    download_url='https://github.com/mahee96/PlayReleaseApi/releases/download/v0.0.1/PlayReleaseApi.zip',
+    download_url='https://github.com/mahee96/PlayReleaseApi/releases/download/v0.0.4/PlayReleaseApi.zip',
     # Keywords that define your package best
     keywords=['Play', 'GooglePlay', 'Console', 'Play Console',
               'python script', 'Github',
@@ -30,17 +30,17 @@ setup(
     install_requires=[            # I get to this in a second
         'requests',
     ],
-    classifiers=[
-        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Development Status :: 3 - Alpha',
-        # Define that your audience are developers
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',   # Again, pick a license
-        # Specify which pyhton versions that you want to support
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.7',
-    ],
+    # classifiers=[
+    #     # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    #     'Development Status :: 4 - Alpha',
+    #     # Define that your audience are developers
+    #     'Intended Audience :: Developers',
+    #     'Topic :: Software Development :: Build Tools',
+    #     'License :: OSI Approved :: MIT License',   # Again, pick a license
+    #     # Specify which pyhton versions that you want to support
+    #     'Programming Language :: Python :: 3',
+    #     'Programming Language :: Python :: 3.4',
+    #     'Programming Language :: Python :: 3.5',
+    #     'Programming Language :: Python :: 3.7',
+    # ],
 )
